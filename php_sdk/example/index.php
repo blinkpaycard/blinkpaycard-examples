15a4fc17e8ca7836ff6f692562bc085ca68d6e5a
finch <finchye@163.com>
2018-12-27 11:53:08 +0800

    修复配置bug

--- php_sdk/example/index.php
+++ php_sdk/example/index.php
@@ -5,9 +5,9 @@ require_once "BlinkPayCard.Api.php";
 $config = new BlinkPayCardConfig;
 $api = new BlinkPayCardApi;
 
-$app_id = $config -> getAppKey();
+$app_id = $config -> getAppId();
 $mch_id = $config -> getMerchantId();
-$app_key = $config -> getAppId();
+$app_key = $config -> getAppKey();
 
 $notify = "{\"nonce_str\":\"0F77ezcval5lEYb6FKPtCMqwMMqzHHLm\"," .
 	"\"amount\":\"100\"," .
@@ -18,18 +18,12 @@ $notify = "{\"nonce_str\":\"0F77ezcval5lEYb6FKPtCMqwMMqzHHLm\"," .
 	"\"sign_type\":\"md5\"," .
 	"\"osu_number\":\"efaa8247f91748468e8ae541e563cb39\"}";
 
-$card_number = "1510000003360001";
-$password = "091086";
-
 echo "=========== 充值下单 ===========" . "\n </br>";
 $pre_order = $api -> preOrder($app_id, $mch_id, $app_key);
 
 echo "=========== 网页充值 =========== " . "\n </br>";
 $webPayUrl = $api -> webPay($mch_id, $pre_order);
 echo "请用浏览器访问：" . $webPayUrl  . "\n </br>";
-
-echo "=========== 接口充值 ===========" . "\n </br>";
-$api -> innerPay($app_id, $mch_id, $app_key, $card_number, $password, $pre_order);
 
 echo "=========== 订单查询 ===========" . "\n </br>";
 $api -> orderQuery($app_id, $mch_id, $app_key, $pre_order);
